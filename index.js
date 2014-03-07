16653d8ac33e043badd8164b55ea836cb5e40f61
Brian Park <yaru22@gmail.com>
2014-03-06 23:40:15 -0500

    Refactored the code.
    The script runs in two phases now.
    1) Collecting issues data (grouped by milestone).
    2) Oututting the data into JSON or CSV format.

--- index.js
+++ index.js
@@ -5,48 +5,113 @@
  *   node ./index.js <repo path> <milestone number 1> <milestone number 2> ...
  *
  *   e.g.
- *   node ./index.js yaru22/hello 1 12 13
+ *   node ./index.js yaru22/github-issue-extractor 1 12 13
  */
-var fs = require('fs'),
+
+var _       = require('lodash'),
+    async   = require('async'),
     request = require('request'),
-    util = require('util');
+    util    = require('util');
 
-var github = require('./configs/github');
+var accessToken;
+try {
+  var githubConfig = require('./configs/github');
+  accessToken = githubConfig.accessToken;
+} catch (err) {
+  accessToken = process.env.GITHUB_ACCESS_TOKEN;
+  if (!accessToken) {
+    console.log('GitHub access token is not available. Please set it via GITHUB_ACCESS_TOKEN environment variable.');
+    process.exit(1);
+  }
+}
 
-var OUTPUT_FILE = 'invoice-tasks.csv',
-    ISSUES_API_URL = 'https://api.github.com/repos/%s/issues?milestone=%s&state=closed&access_token=%s';
+var ISSUES_API_URL = 'https://api.github.com/repos/%s/issues?milestone=%s&state=closed&access_token=%s';
 
-var repoPath = process.argv[2],
-    rowCount = 1;
+var repoPath         = process.argv[2],
+    milestoneNumbers = process.argv.slice(3);
 
-fs.writeFileSync(OUTPUT_FILE, ',Issue #,Title,Total Hours,Invoice Item\n');
-rowCount++;
+function collectIssues(outputFunction) {
+  async.map(milestoneNumbers, function (milestone, done) {
+    var issuesUrl = util.format(ISSUES_API_URL, repoPath, milestone, accessToken);
+    request({
+      url: issuesUrl,
+      json: true,
+      headers: {
+        'User-Agent': 'Node.js Request'
+      }
+    }, function (err, resp, body) {
+      if (err || resp.statusCode !== 200) {
+        console.log(err, resp);
+        return;
+      }
 
-process.argv.slice(3).map(function (milestone) {
-  var issuesUrl = util.format(ISSUES_API_URL, repoPath, milestone, github.accessToken);
-  request({
-    url: issuesUrl,
-    json: true,
-    headers: {
-      'User-Agent': 'Node.js Request'
+      var milestoneObj = {};
+      milestoneObj.title = body[0].milestone.title;
+      milestoneObj.issues = [];
+
+      body.map(function (issue) {
+        var estimateMatcher = /total estimate hours: (\d+(\.\d+)?) hours/m;
+
+        var issueNumber = issue.number,
+            issueTitle = issue.title.replace(/"/g, '\''),
+            issueEstimate = issue.body.toLowerCase().match(estimateMatcher);
+        if (issueEstimate) {
+          issueEstimate = parseFloat(issueEstimate[1]);
+        }
+
+        milestoneObj.issues.push({
+          number: issueNumber,
+          title: issueTitle,
+          estimate: issueEstimate
+        });
+      });
+
+      return done(null, milestoneObj);
+    });
+  }, function (err, results) {
+    if (err) {
+      console.log(err);
     }
-  }, function (err, resp, body) {
-    if (err || resp.statusCode !== 200) {
-      console.error(err, resp);
-      return;
-    }
+    outputFunction(results);
+  });
+}
 
-    console.log('Fetched milestone', milestone);
-    fs.appendFileSync(OUTPUT_FILE, util.format('"%s",,,,\n', body[0].milestone.title));
+var outputFunctions = {};
+
+outputFunctions.all = function (data) {
+  console.log('===== JSON format =====');
+  outputFunctions.json(data);
+  console.log('\n===== CSV format =====');
+  outputFunctions.csv(data);
+};
+
+outputFunctions.json = function (data) {
+  console.log(JSON.stringify(data, null, 2));
+};
+
+outputFunctions.csv = function (data) {
+  var rowCount = 1;
+
+  // Print the header.
+  console.log(',Issue #,Title,Total Hours,Invoice Item');
+  rowCount++;
+
+  _.each(data, function (milestone) {
+    console.log(util.format('"%s",,,,', milestone.title));
     rowCount++;
 
-    body.map(function (issue) {
-      fs.appendFileSync(OUTPUT_FILE, util.format(',"%s","%s",,"=""#"" & B%s & "": "" & C%s"\n',
-          issue.number,
-          issue.title.replace(/"/g, '\''),
-          rowCount,
-          rowCount));
+    _.each(milestone.issues, function (issue) {
+      console.log(
+        util.format(',"%s","%s","%s","=""#"" & B%s & "": "" & C%s"',
+            issue.number,
+            issue.title,
+            issue.estimate,
+            rowCount,
+            rowCount)
+      );
       rowCount++;
     });
   });
-});
+};
+
+collectIssues(outputFunctions.all);
